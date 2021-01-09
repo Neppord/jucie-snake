c3770a7ee549f5520e8b50245e3c7661297de7eb
Samuel Ytterbrink <neppord@gmail.com>
2021-01-09 17:52:00 +0100

    Paint snake segment

--- src/Main.elm
+++ src/Main.elm
@@ -1,7 +1,28 @@
 module Main exposing (main)
 
-import Html exposing (text)
+import Color
+import TypedSvg exposing (rect, svg)
+import TypedSvg.Attributes exposing (fill)
+import TypedSvg.Attributes.InPx
+    exposing
+        ( height
+        , width
+        , x
+        , y
+        )
+import TypedSvg.Types exposing (Paint(..))
 
 
 main =
-    text "Hello"
+    svg [] [ snakeBody 0 0 ]
+
+
+snakeBody xPos yPos =
+    rect
+        [ width 25
+        , height 25
+        , x <| xPos * 25
+        , y <| yPos * 25
+        , fill <| Paint Color.green
+        ]
+        []
