6334f054b06fa842b42efb0b9e7224b9da34525c
Samuel Ytterbrink <neppord@gmail.com>
2021-01-09 22:41:35 +0100

    Add more body segments

--- src/Main.elm
+++ src/Main.elm
@@ -2,11 +2,12 @@ module Main exposing (main)
 
 import Browser exposing (document)
 import Color
-import Css exposing (animationDuration, animationName, ms, sec)
+import Css exposing (animationDuration, animationName, ms, sec, vh, vmin)
 import Css.Animations exposing (custom, keyframes, property)
 import Css.Global exposing (global)
 import Css.Transitions exposing (transition)
 import Html.Styled exposing (toUnstyled)
+import List.Extra
 import TypedSvg exposing (rect, svg)
 import TypedSvg.Attributes exposing (class, fill)
 import TypedSvg.Attributes.InPx
@@ -23,16 +24,64 @@ main =
     document
         { init = \() -> ( (), Cmd.none )
         , view = always view
-        , update = \_ model -> ( model, Cmd.none )
+        , update = \_ m -> ( m, Cmd.none )
         , subscriptions = always Sub.none
         }
+
+
+type alias Point =
+    ( Int, Int )
+
+
+type alias Snake =
+    { head : Point
+    , tail : List Direction
+    }
+
+
+type Direction
+    = Up
+    | Down
+    | Left
+    | Right
+
+
+move : Direction -> Point -> Point
+move direction ( xPos, yPos ) =
+    case direction of
+        Up ->
+            ( xPos, yPos - 1 )
+
+        Down ->
+            ( xPos, yPos + 1 )
+
+        Left ->
+            ( xPos - 1, yPos )
+
+        Right ->
+            ( xPos + 1, yPos )
+
+
+toCords snake =
+    snake.head :: List.Extra.scanl move snake.head snake.tail
+
+
+model =
+    { snake =
+        { head = ( 5, 5 )
+        , tail = [ Right, Right, Right, Right, Down, Down, Left ]
+        }
+    }
 
 
 view =
     { title = "Juice Snake"
     , body =
         [ toUnstyled css
-        , svg [] [ snakeBody 0 0 ]
+        , model.snake
+            |> toCords
+            |> List.map snakeBody
+            |> svg []
         ]
     }
 
@@ -51,15 +100,19 @@ css =
             , animationDuration <| ms 500
             , Css.property "animation-timing-function" "ease-out"
             ]
+        , Css.Global.svg
+            [ Css.width <| vmin 100
+            , Css.height <| vmin 100
+            ]
         ]
 
 
-snakeBody xPos yPos =
+snakeBody ( xPos, yPos ) =
     rect
         [ width 25
         , height 25
-        , x <| xPos * 25
-        , y <| yPos * 25
+        , x <| toFloat xPos * 25
+        , y <| toFloat yPos * 25
         , fill <| Paint Color.green
         , class [ "snake-body" ]
         ]
